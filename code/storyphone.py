4630415afa613d45ac9608424acbb46833cb1621
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-12-13 17:10:20 -0600

    put the files back

---
+++ code/storyphone.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import subprocess
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(1, GPIO.IN)
+
+proc = None 
+
+while True:
+    if (GPIO.input(1) == True):
+	print("off the hook!")
+	if proc:
+            print("playing")
+		#proc.terminate()
+		#proc = None
+	else:
+            proc = subprocess.Popen(['mpg123', '-q', './media/381030.mp3'])
+    else:
+        print("on the hook")
+
+        if proc:
+            proc.terminate()
+            proc = None
+
+    sleep(.5);
