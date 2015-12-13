7dc52b4262a2abed140bfe65881a2007eee3e7e9
Jason J. Gullickson <jason.gullickson@gmail.com>
2015-12-13 22:57:48 +0000

    playack when off-hook, stop when on-hook

--- storyphone.py
+++ storyphone.py
@@ -11,12 +11,19 @@ GPIO.setup(1, GPIO.IN)
 proc = None 
 
 while True:
-    if (GPIO.input(1) == False):
+    if (GPIO.input(1) == True):
 	print("off the hook!")
 	if proc:
-		proc.terminate()
-		proc = None
+            print("playing")
+		#proc.terminate()
+		#proc = None
 	else:
-		proc = subprocess.Popen(['mpg123', '-q', '381030.mp3'])
+            proc = subprocess.Popen(['mpg123', '-q', '381030.mp3'])
+    else:
+        print("on the hook")
+
+        if proc:
+            proc.terminate()
+            proc = None
 
     sleep(.5);
